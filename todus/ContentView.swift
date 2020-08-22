24d800c3f697976d8a19d28a630d8e6b744c9f07
Asiel Cabrera <cabrerasiel@gmail.com>
2020-08-21 23:06:13 -0400

    Initial Commit

---
+++ todus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todus
+//
+//  Created by Asiel Cabrera on 8/21/20.
+//  Copyright © 2020 Asiel Cabrera. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
